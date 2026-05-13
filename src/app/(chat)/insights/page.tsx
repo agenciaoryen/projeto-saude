c8421aac7cdfb6568ecb319475e3a360a31ddca8
agenciaoryen <agenciaoryen@gmail.com>
2026-05-12 22:57:49 -0300

    Input chat muito mais alto: pb-20 + padding interno

--- src/app/(chat)/insights/page.tsx
+++ src/app/(chat)/insights/page.tsx
@@ -245,7 +245,7 @@ export default function MayaChatPage() {
       </div>
 
       {/* Input bar */}
-      <div className="shrink-0 px-2 pt-2 pb-10 bg-background border-t border-border safe-bottom">
+      <div className="shrink-0 px-3 pt-3 pb-20 bg-background border-t border-border safe-bottom">
         <div className="flex items-end gap-2">
           <textarea
             ref={textareaRef}
